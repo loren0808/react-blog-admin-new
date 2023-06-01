bb0558fc34da4e0287450cdab804a13091f1fbd6
haorh <haorh@mail.ustc.edu.cn>
2023-06-01 22:45:46 +0800

    dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { vitePluginForArco } from '@arco-plugins/vite-react'
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vitePluginForArco } from '@arco-plugins/vite-react';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePluginForArco(),
-  ],
+  plugins: [react(), vitePluginForArco()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
